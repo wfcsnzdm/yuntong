bbfbd2246fc2d48a32772c28b8a11b7a701ee8c5
root <root@Ecjtu.(none)>
2015-07-23 19:02:40 +0800

    push

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'yuntong_ecjtu_net';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'EcjtuNet7#214_51';
+$cfg_dbprefix = 'yuntong_';
+$cfg_db_language = 'utf8';
+
+
+?>
